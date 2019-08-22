f0b0cd92c1fa549e0f3a71f093f145e4c2b83145
Avril Harder <54415474+avril-m-harder@users.noreply.github.com>
2019-08-22 16:47:12 -0400

    Update stringtie_example.sh

--- stringtie_example.sh
+++ stringtie_example.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#PBS -N stringtie_1A
+#PBS -N stringtie
 #PBS -q darwin
 #PBS -l nodes=1:ppn=4,naccesspolicy=singleuser
 #PBS -l walltime=300:00:00
-#PBS -m ae
-#PBS -M avrilharder@gmail.com
 
 cd $PBS_O_WORKDIR
 
@@ -23,3 +21,17 @@ stringtie /scratch/snyder/h/harder/analyses_ssalar_rna_seq/hisat2/hisat2_13a2.so
 stringtie /scratch/snyder/h/harder/analyses_ssalar_rna_seq/hisat2/hisat2_13-a.sorted.bam -l 13-a -p 4 -G /scratch/snyder/h/harder/ssalar_genome_and_annotation_files/use_this_one_ref_ICSASG_v2_top_level.gtf -o hisat2_13-a.sorted.bam.gtf
 stringtie /scratch/snyder/h/harder/analyses_ssalar_rna_seq/hisat2/hisat2_13-b.sorted.bam -l 13-b -p 4 -G /scratch/snyder/h/harder/ssalar_genome_and_annotation_files/use_this_one_ref_ICSASG_v2_top_level.gtf -o hisat2_13-b.sorted.bam.gtf
 stringtie /scratch/snyder/h/harder/analyses_ssalar_rna_seq/hisat2/hisat2_13b.sorted.bam -l 13b -p 4 -G /scratch/snyder/h/harder/ssalar_genome_and_annotation_files/use_this_one_ref_ICSASG_v2_top_level.gtf -o hisat2_13b.sorted.bam.gtf
+
+
+## once all sample assemblies have been generated:
+
+# merge all transcripts from all samples + reference annotation
+stringtie --merge -p 4 \
+-G /scratch/snyder/h/harder/ssalar_genome_and_annotation_files/use_this_one_ref_ICSASG_v2_top_level.gtf \
+-o stringtie_all_merged.gtf mergelist.txt
+
+
+# compare assembled transcripts to known transcripts
+gffcompare -r \
+/scratch/snyder/h/harder/ssalar_genome_and_annotation_files/use_this_one_ref_ICSASG_v2_top_level.gtf \
+-G -o stringtie_all_merged stringtie_all_merged.gtf
